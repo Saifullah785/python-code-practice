d2115a6b28e6badac53e73383fda63c2f26a0a57
Saifullah785 <muhammadalisaif785@gmail.com>
2025-04-21 13:56:08 +0500

    Add files via upload

---
+++ Lecture_03/Lecture_03.py
@@ -0,0 +1,11 @@
+# 1.WAP to ask the user to enter names of their 3 favorite movies & store them in a list.
+
+
+movies = []
+movie1 = input("Enter your first favorite movie: ")
+movie2 = input("Enter your second favorite movie: ")
+movie3 = input("Enter your third favorite movie: ")
+movies.append(movie1)
+movies.append(movie2)
+movies.append(movie3)
+print("Your favorite movies are: ", movies)
