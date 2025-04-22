537acf3ece26f2ff7a7b97b8d933f7eebda64e79
Saifullah785 <muhammadalisaif785@gmail.com>
2025-04-22 11:10:25 +0500

    Add files via upload

---
+++ Lecture_05/Lecture_05.py
@@ -0,0 +1,14 @@
+
+# while loop
+
+n = int(input("Enter a number: "))
+i=1
+while i <= 10:
+    print(n*i)
+    i += 1
+
+# range()
+
+n = int(input("Enter a number: "))
+for i in range(1, 11):
+    print(n*i)
